765f6d4a0d7752a55b15ff44d1ecfaaf7fd145a0
wrobell <wrobell@pld-linux.org>
2014-04-08 23:24:16 +0100

    - readded nose collector

--- setup.py
+++ setup.py
@@ -45,7 +45,8 @@ frames.
     ],
     keywords='gis',
     license='GPL',
-    install_requires = ['shapely >= 1.3', 'pandas >= 0.13.1']
+    install_requires = ['shapely >= 1.3', 'pandas >= 0.13.1'],
+    test_suite='nose.collector',
 )
 
 # vim: sw=4:et:ai
